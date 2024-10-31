cdb17d025c16a45bd22980981642ee0102555a08
pongthorn <oumail002@gmail.com>
2024-10-31 10:14:25 +0700

    Update testme.go

--- testme.go
+++ testme.go
@@ -10,26 +10,40 @@ type EmpItem struct {
 	Address string `json:"Address"`
 }
 
+type Movie struct {
+	ID int64 `json:"id"`
+}
+
 func main() {
-	var empItem EmpItem
-	emp := make([]EmpItem, 0)
 
-	empItem.Id = "1"
-	empItem.Name = "FirstName1"
-	empItem.Address = "Address1"
-	emp = append(emp, empItem)
+	m := &Movie{}
+	resultShow := [2]Movie{}
+	m.ID = 1
+	resultShow[0].ID = m.ID
+	fmt.Printf("%#v\n", resultShow)
+	m.ID = 2
+	resultShow[1].ID = m.ID
+	fmt.Printf("%#v\n", resultShow)
 
-	empItem.Id = "2"
-	empItem.Name = "FirstName2"
-	empItem.Address = "Address2"
-	emp = append(emp, empItem)
+	// var empItem EmpItem
+	// emp := make([]EmpItem, 0)
 
-	// jsonString, _ := json.Marshal(emp)
+	// empItem.Id = "1"
+	// empItem.Name = "FirstName1"
+	// empItem.Address = "Address1"
+	// emp = append(emp, empItem)
 
-	// fmt.Printf("%s", jsonString)
+	// empItem.Id = "2"
+	// empItem.Name = "FirstName2"
+	// empItem.Address = "Address2"
+	// emp = append(emp, empItem)
 
-	var ss []int
-	if ss == nil {
-		fmt.Printf("%#v", ss)
-	}
+	// // jsonString, _ := json.Marshal(emp)
+
+	// // fmt.Printf("%s", jsonString)
+
+	// var ss []int
+	// if ss == nil {
+	// 	fmt.Printf("%#v", ss)
+	// }
 }
